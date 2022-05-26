538644b01e5fbf75ad82332abc2c8a968a628d06
linusaxel <linusaxel@gmail.com>
2022-05-26 14:03:06 +0200

    working tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,7 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
-const Home: NextPage = () => {
+export default function Home() {
   return (
-    <div className={styles.container}>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>Hey cutieeeee</h1>
-      </main>
+    <div className="text-red-500 text-2xl">
+      Hello world!
     </div>
   )
 }
-
-export default Home
